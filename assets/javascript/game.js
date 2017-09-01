45df65bc66a76fa471549b6f7cf6bfc02550cefb
alexdecycive <alex@decycive.com>
2017-09-01 15:17:52 -0400

    all working and ready to go!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,15 @@
 $(document).ready(function() {
 	console.log("Game has started")
 
-
 var losses = 0;
 var wins = 0;
 var yourscore = 0;
+
 
 // create the random number to start the game on page load (19 to 120)
 var randomnumber = Math.floor(Math.random() * 100) + 19;
 console.log(randomnumber);
 $("#randomnum-div").text(randomnumber);
-
 
 
 // function to assign new point values to each gem on function invocation (1 to 12 points each)
@@ -26,7 +25,6 @@ console.log("Gem 3 = " + gem3);
 
 var gem4 = Math.floor(Math.random() * 12) + 1;
 console.log("Gem 4 = " + gem4);
-
 
 
 // save the points from each gem to YOUR SCORE on each gem click
@@ -63,22 +61,48 @@ $("#gemimg4").on("click", function() {
 function winner() {
 if (yourscore === randomnumber) {
 	console.log("You Won");
-	$("#winloseline").text("WON");
+	window.alert("You won! Great work. Play again. Your score will reset now.");
 	wins++;
 	$("#wins-div").text(wins);
+	reset();
 }};
 
 
 // Loser function to save losses and update DOM
 function loser() {
 	if (yourscore > randomnumber) {
-	console.log("you lost");
-	$("#winloseline").text("LOST");
+	console.log("You Lost");
+	window.alert("You lost. Please try again. Your score will reset now.");
 	losses++;
 	$("#losses-div").text(losses);
+	reset();
 }};
 
+
 //game reset function that keeps wins and losses but sets new random num and gem values
+function reset() {
+	console.log("Game reset")
+	yourscore = 0;
+	$("#userscore").text(yourscore);
+
+	randomnumber = 0;
+	randomnumber = Math.floor(Math.random() * 100) + 19;
+	console.log(randomnumber);
+	$("#randomnum-div").text(randomnumber);
+
+	var gem1 = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 1 = " + gem1);
+
+	var gem2 = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 2 = " + gem2);
+
+	var gem3 = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 3 = " + gem3);
+
+	var gem4 = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 4 = " + gem4);
+
+}
 
 
 //End the document ready function
